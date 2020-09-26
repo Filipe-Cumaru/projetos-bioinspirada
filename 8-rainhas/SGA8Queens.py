1c1e3798c5cb29f65253bb6c53c5bc0064170959
Filipe Cumaru <facsa@cin.ufpe.br>
2020-09-26 18:28:30 -0300

    refactor: Reorganizing main class.
    Reorganizing main class to allow generic behaviour and custom setup.
    This commit adds attributes to select the chromossome representation,
    recombination, mutation, survivor selection, and parent selection
    methods.

--- 8-rainhas/SGA8Queens.py
+++ 8-rainhas/SGA8Queens.py
@@ -1,7 +1,14 @@
 import numpy as np
 
 class SGA8Queens(object):
-    def __init__(self, crossover_prob=0.9, mutation_prob=0.4, pop_size=100, use_binary_rep=False):
+    def __init__(self, crossover_prob=0.9, \
+                    mutation_prob=0.4, \
+                    pop_size=100, \
+                    use_binary_rep=False, \
+                    recombination_method=1, \
+                    mutation_method=1, \
+                    parents_sel_method=1, \
+                    survivor_sel_method=1):
         # Crossover probability.
         self.p_c = crossover_prob
 
@@ -18,6 +25,42 @@ class SGA8Queens(object):
 
         # Use binary string representation for population.
         self.use_binary_rep = use_binary_rep
+
+        # Set recombination method. Uses cut and crossfill for 1.
+        # TODO: add other methods.
+        if recombination_method == 1:
+            self.recombine = self.cut_and_crossfill_crossover
+        else:
+            raise ValueError("Invalid option value for the recombination method.")
+        
+        # Set mutation method. 1 for gene switch.
+        # TODO: add other methods.
+        if mutation_method == 1:
+            self.mutate = self.gene_switch_mutation
+        else:
+            raise ValueError("Invalid option value for the mutation method.")
+        
+        # Set parent selection method. Value 1 for best 2 out of random 5, and 2
+        # for roulette.
+        if parents_sel_method == 1:
+            self.select_parents = self.select_2_out_5
+        elif parents_sel_method == 2:
+            self.select_parents = self.roulette_selection
+        else:
+            raise ValueError("Invalid option value for the parents selection method.")
+
+        # Set survivor selection method. Value 1 for replacement of the worst
+        # strategy, and 2 for generation approach.
+        if survivor_sel_method == 1:
+            self.select_survivors = self.replace_worst
+        elif parents_sel_method == 2:
+            self.select_survivors = self.generational
+        else:
+            raise ValueError("Invalid option value for the survivor selection method.")
+
+    # #########################
+    # COMMON METHODS AND UTILS
+    # #########################
 
     def fitness(self, table):
         # Based on https://bit.ly/32zycds.
@@ -53,59 +96,6 @@ class SGA8Queens(object):
         
         return 1 / (result + eps)
 
-    def select_parents(self):
-        # Numpy's random number generator.
-        rng = np.random.default_rng()
-        # Get 5 different random individuals from the population.
-        candidates = rng.choice(self.population, size=5, replace=False)
-        # Find the two candidates with the highest fitness.
-        candidates_fitness = np.array([self.fitness(x) for x in candidates])
-        p1, p2 = candidates[np.argsort(-candidates_fitness)[:2]]
-        p1, p2 = list(p1), list(p2)
-        return p1, p2
-
-    def cut_and_crossfill_crossover(self, p1, p2):
-        # Implementation of a cut and crossfill crossover.
-        rng = np.random.default_rng()
-        c1 = 8*['']
-        c2 = 8*['']
-
-        if rng.uniform() < self.p_c:
-            i = rng.choice(8)
-            c1[:i], c2[:i] = p1[:i], p2[:i]
-            it1, it2 = i, i
-            for j in range(8):
-                if p2[j] not in c1:
-                    c1[it1] = p2[j]
-                    it1 += 1
-                if p1[j] not in c2:
-                    c2[it2] = p1[j]
-                    it2 += 1
-        else:
-            c1, c2 = p1[:], p2[:]
-        
-        return c1, c2
-
-    def mutate(self, child):
-        rng = np.random.default_rng()
-
-        # Mutation by switching the position of a two genes.
-        if rng.uniform() < self.p_m:
-            i, j = rng.choice(8, size=2, replace=False)
-            child[i], child[j] = child[j], child[i]
-
-        return child
-
-    def update_population(self, c1, c2):
-        # Find the two individuals with the lowest fitness value.
-        weakest_solutions = np.argsort(self.pop_fitness)[:2]
-        # Replace them by the childs.
-        self.population[weakest_solutions[0]] = c1[:]
-        self.population[weakest_solutions[1]] = c2[:]
-        # Update the fitness table.
-        self.pop_fitness[weakest_solutions[0]] = self.fitness(c1)
-        self.pop_fitness[weakest_solutions[1]] = self.fitness(c2)
-    
     def random_init_population(self):
         rng = np.random.default_rng()
 
@@ -129,12 +119,97 @@ class SGA8Queens(object):
 
         while self.num_fitness_eval < 10000 and self.pop_fitness.max() < 1:
             p1, p2 = self.select_parents()
-            c1, c2 = self.cut_and_crossfill_crossover(p1, p2)
+            c1, c2 = self.recombine(p1, p2)
             c1 = self.mutate(c1)
             c2 = self.mutate(c2)
-            self.update_population(c1, c2)
+            self.select_survivors(c1, c2)
 
         report = {'num fitness eval': self.num_fitness_eval, \
             'solution': self.population[self.pop_fitness.argmax()]}
         
         return report
+    
+    # ########################
+
+    # #########################
+    # PARENT SELECTION METHODS
+    # #########################
+
+    def select_2_out_5(self):
+        # Numpy's random number generator.
+        rng = np.random.default_rng()
+        # Get 5 different random individuals from the population.
+        candidates = rng.choice(self.population, size=5, replace=False)
+        # Find the two candidates with the highest fitness.
+        candidates_fitness = np.array([self.fitness(x) for x in candidates])
+        p1, p2 = candidates[np.argsort(-candidates_fitness)[:2]]
+        p1, p2 = list(p1), list(p2)
+        return p1, p2
+    
+    def roulette_selection(self):
+        pass
+
+    # #########################
+
+    # ######################
+    # RECOMBINATION METHODS
+    # ######################
+
+    def cut_and_crossfill_crossover(self, p1, p2):
+        # Implementation of a cut and crossfill crossover.
+        rng = np.random.default_rng()
+        c1 = 8*['']
+        c2 = 8*['']
+
+        if rng.uniform() < self.p_c:
+            i = rng.choice(8)
+            c1[:i], c2[:i] = p1[:i], p2[:i]
+            it1, it2 = i, i
+            for j in range(8):
+                if p2[j] not in c1:
+                    c1[it1] = p2[j]
+                    it1 += 1
+                if p1[j] not in c2:
+                    c2[it2] = p1[j]
+                    it2 += 1
+        else:
+            c1, c2 = p1[:], p2[:]
+        
+        return c1, c2
+    
+    # #########################
+
+    # #################
+    # MUTATION METHODS
+    # #################
+
+    def gene_switch_mutation(self, child):
+        rng = np.random.default_rng()
+
+        # Mutation by switching the position of a two genes.
+        if rng.uniform() < self.p_m:
+            i, j = rng.choice(8, size=2, replace=False)
+            child[i], child[j] = child[j], child[i]
+
+        return child
+
+    # #########################
+
+    # ###########################
+    # SURVIVOR SELECTION METHODS
+    # ###########################
+
+    def replace_worst(self, c1, c2):
+        # Find the two individuals with the lowest fitness value.
+        weakest_solutions = np.argsort(self.pop_fitness)[:2]
+        # Replace them by the childs.
+        self.population[weakest_solutions[0]] = c1[:]
+        self.population[weakest_solutions[1]] = c2[:]
+        # Update the fitness table.
+        self.pop_fitness[weakest_solutions[0]] = self.fitness(c1)
+        self.pop_fitness[weakest_solutions[1]] = self.fitness(c2)
+
+    def generational(self, childs):
+        pass
+
+    # #########################
