02a4d539aa61902a38352005d371ad74630a2078
Filipe Cumaru <facsa@cin.ufpe.br>
2020-10-19 11:46:30 -0300

    chore: rename files

---
+++ ackley/AckleyES.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class AckleyES(object):
+    def __init__(self):
+        pass
+
+    def recombine(self, parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def mutate(self, parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def select_offspring(self, parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def select_parents(self, parameter_list):
+        """
+        docstring
+        """
+        pass
--- ackley/AckleyEvolutionaryStrategy.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-class AckleyES(object):
-  def __init__(self):
-    pass
